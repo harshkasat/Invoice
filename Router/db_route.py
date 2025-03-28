c0c8e4bb9beaa1cfad4a0aa3ae35fc9883b51850
Zedmat <104870914+harshkasat@users.noreply.github.com>
2025-03-28 20:31:35 +0530

    update the router section

--- Router/db_route.py
+++ Router/db_route.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
+import tempfile
+from fastapi import APIRouter, Response, UploadFile, File
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-from DB.db_operations import PDFModel, UserManager
+from DB.db_operations import UserManager
+from utils.file_manager import PDFService
 from typing import Optional
 from uuid import UUID
 
@@ -32,6 +34,7 @@ async def create_user(user_info:UserModel):
     return JSONResponse(get_user)
 
 
+# User Manager
 @router.get('/list_user', tags=["User Manager"])
 async def list_user():
     user = UserManager()
@@ -48,5 +51,32 @@ async def get_user_by_email(email:str):
 async def delete_user(user_id:UUID):
     user = UserManager()
     deleted_user = user.delete_user(user_id=user_id)
-    return JSONResponse(delete_user)
+    return JSONResponse(deleted_user)
 
+
+# PDF File Manager
+@router.get('/list_pdf', tags=["PDF Manager"])
+async def list_pdf(user_id:UUID):
+    pdf_service = PDFService()
+    pdf_list = pdf_service.list_pdfs_by_user_id(user_id=user_id)
+    return pdf_list
+
+@router.post('/create_pdf', tags=["PDF Manager"])
+async def create_pdf(user_id:UUID, file_path:UploadFile = File(...)):
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        content = await file_path.read()
+        temp_file.write(content)
+        temp_file_path = temp_file.name
+    pdf_service = PDFService()
+    secure_url, public_id, original_filename = pdf_service.save_pdf(
+        user_id=user_id,
+        file_path=temp_file_path,
+        file_name=file_path.filename
+        )
+    return JSONResponse(
+        content={
+            "message": "File uploaded and processing started", 
+            "secure_url": secure_url,
+            "public_id": public_id,
+            "filename": original_filename
+        })
