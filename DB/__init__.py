c525ed3448feb4b8cce07f17f24e83aaae9bef42
Zedmat <104870914+harshkasat@users.noreply.github.com>
2025-05-03 20:02:02 +0530

    by mistak hardcode db url

--- DB/__init__.py
+++ DB/__init__.py
@@ -2,8 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# DATABASE_URL = os.getenv("SUPABASE_DATABASE_URL")
-DATABASE_URL = 'postgresql://postgres.qwvnfuwovmdhdnnuzklt:uvuFojwlS3mtK3h4@aws-0-ap-south-1.pooler.supabase.com:6543/postgres'
+DATABASE_URL = os.getenv("SUPABASE_DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("SUPABASE_DATABASE_URL environment variable is not set.")
 
